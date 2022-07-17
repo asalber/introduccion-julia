779ec4c82902e44b523abd867c7db85702ca2130
Alfredo Sánchez Alberca <asalber@gmail.com>
2022-07-18 00:47:16 +0200

    Añadidas secciones de cálculo simbólico y análisis de datos.

--- prueba.jl
+++ prueba.jl
@@ -1,9 +1,7 @@
-function uno(x)
-    x += 1
-    function dos(y)
-        y *= 2
-    end
-    return dos(x)
-end
+using Plots; gr()
 
-uno(1)
+x = range(0, 10, length=30)
+surface(
+  x, x, (x, y)->sin(x)+cos(y), c=:viridis, legend=:none,
+  nx=50, ny=50, display_option=Plots.GR.OPTION_SHADED_MESH,  # <-- series[:extra_kwargs]
+)
